cb8a918852c9c44f288a07a9e560ae687c90505a
CarolineAloca <81122964+CarolineAloca@users.noreply.github.com>
2026-01-23 19:07:10 -0300

    Add files via upload

--- cenas/getulio_static.gd
+++ cenas/getulio_static.gd
@@ -2,6 +2,6 @@ extends StaticBody2D
 
 @onready var dialog_box: Control = $Dialogo_3/DialogBox_3
 
-func interact():
-	print("INTERACT NA ESTÁTUA")
+func interact(_player = null):
+	print("INTERACT FOI CHAMADO")
 	dialog_box.next()
