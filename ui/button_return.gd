d083692afce791ad3980b2e142d54554f6ddc950
CarolineAloca <81122964+CarolineAloca@users.noreply.github.com>
2026-02-10 15:00:49 -0300

    Add files via upload

--- ui/button_return.gd
+++ ui/button_return.gd
@@ -1,7 +1,12 @@
 extends Button
 
+@onready var click_sound: AudioStreamPlayer = $"../ClickSound"
+
 func _on_pressed() -> void:
+	click_sound.play()
+	await click_sound.finished
 	get_tree().change_scene_to_file("res://ui/menu.tscn")
+
 
 
 func _on_button_quit_pressed() -> void:
